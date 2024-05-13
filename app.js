b1f8bb785ad3cf1f8b3879dd06296bc90434a8b4
Apoorv Singh <apoorv3591@gmail.com>
2024-05-14 03:09:46 +0530

    test6

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route to handle GET requests to the root URL '/'
 app.get('/', (req, res) => {
-    res.send('sample test 6');
+    res.send('sample test 7');
 });
 
 // Start the server and listen on port 3000
