762e5eda558c2581077e917ebaf80876763e9c50
Apoorv Singh <apoorv3591@gmail.com>
2024-05-14 11:22:01 +0530

    pipeline test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route to handle GET requests to the root URL '/'
 app.get('/', (req, res) => {
-    res.send('auto deploy jenkins');
+    res.send('jenkins pipeline test');
 });
 
 // Start the server and listen on port 3000
